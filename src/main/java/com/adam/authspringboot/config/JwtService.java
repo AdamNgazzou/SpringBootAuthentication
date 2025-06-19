ca6911f90ce760fbae870ade206437f60ba672aa
AdamNgazzou <adem.ng123@gmail.com>
2025-06-19 15:00:07 +0100

    adding jwt token service and repository

---
+++ src/main/java/com/adam/authspringboot/config/JwtService.java
@@ -0,0 +1,23 @@
+package com.adam.authspringboot.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
