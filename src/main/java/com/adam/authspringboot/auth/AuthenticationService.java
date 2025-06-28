37c47fa0a712540af59165cc246740de35fba8dc
AdamNgazzou <adem.ng123@gmail.com>
2025-06-28 13:16:39 +0100

    i did fix a huge mistake forgetting 'final' for the userDetails service injection , did document the http endpoints and tested everything hihihi

--- src/main/java/com/adam/authspringboot/auth/AuthenticationService.java
+++ src/main/java/com/adam/authspringboot/auth/AuthenticationService.java
@@ -1,10 +1,13 @@
 package com.adam.authspringboot.auth;
 
 
+import com.adam.authspringboot.config.JwtService;
 import com.adam.authspringboot.user.Role;
 import com.adam.authspringboot.user.User;
 import com.adam.authspringboot.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,10 @@ public class AuthenticationService {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
     public AuthenticationResponse register(RegisterReqeust request) {
         var user = User.builder()
                 .firstName(request.getFirstName())
@@ -25,10 +32,25 @@ public class AuthenticationService {
                 .role(Role.USER)
             .build();
         userRepository.save(user);
-        return AuthenticationResponse.builder().token("token").build();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        // if i get to this point then the authentication was successful
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
